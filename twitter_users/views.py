94a6876f80572e5b6ddb680957c52e7920a19f6b
Jason Cernansky <jason@iradix.com>
2015-07-15 13:27:55 -0400

    Added an exception handler
    Added an exception handler to catch when twitter throws an exception

--- twitter_users/views.py
+++ twitter_users/views.py
@@ -43,24 +43,28 @@ def twitter_login(request, redirect_field_name='next'):
 
 def twitter_callback(request):
     if not 'denied' in request.GET:
-        oauth_token    = request.GET['oauth_token']
-        oauth_verifier = request.GET['oauth_verifier']
-        # get an access token from Twitter
-        logger.debug("Requesting oauth consumer token with: %s, %s" % (settings.KEY, settings.SECRET))
-        consumer           = oauth.Consumer(settings.KEY, settings.SECRET)
-        logger.debug("Requesting oauth access token with: %s, %s, %s" % (consumer, oauth_token, oauth_verifier))
-        access_token       = oauth.AccessToken(consumer, oauth_token, oauth_verifier)
-        logger.debug("Received %s" % access_token)
+        try:
+            oauth_token    = request.GET['oauth_token']
+            oauth_verifier = request.GET['oauth_verifier']
+            # get an access token from Twitter
+            logger.debug("Requesting oauth consumer token with: %s, %s" % (settings.KEY, settings.SECRET))
+            consumer           = oauth.Consumer(settings.KEY, settings.SECRET)
+            logger.debug("Requesting oauth access token with: %s, %s, %s" % (consumer, oauth_token, oauth_verifier))
+            access_token       = oauth.AccessToken(consumer, oauth_token, oauth_verifier)
+            logger.debug("Received %s" % access_token)
         
-        # actually log in
-        user = authenticate(twitter_id  = access_token.user_id,
-                            username    = access_token.username,
-                            token       = access_token.token,
-                            secret      = access_token.secret)
-        login(request, user)
-    
-        # redirect to the authenticated view
-        redirect_to = request.session['redirect_to']
+            # actually log in
+            user = authenticate(twitter_id  = access_token.user_id,
+                                username    = access_token.username,
+                                token       = access_token.token,
+                                secret      = access_token.secret)
+            login(request, user)
+            # redirect to the authenticated view
+            redirect_to = request.session['redirect_to']
+        except:
+            redirect_to = '/'
+        
+        
     else:
         redirect_to = '/'
     if not redirect_to or not is_safe_redirect(redirect_to):
